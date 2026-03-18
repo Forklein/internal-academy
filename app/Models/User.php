425dfd8c2669415b1ebd10ac6958dd135c288b81
forklein <giuseppepisani2@libero.it>
2026-03-18 15:50:59 +0100

    update model and add role_id to users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Role;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    //Relazione con Role molti a 1
+    public function role(){
+        return $this->belongsTo(Role::class);
+    }
+    //Check if user is admin
+    public function isAdmin() {
+        return $this->role?->name === 'admin';
     }
 }
