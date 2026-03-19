fc56947f9a92e052cc900beee46ee7b40d6a7b5a
forklein <giuseppepisani2@libero.it>
2026-03-19 15:25:49 +0100

    Register middleware in app and add it in auth

--- routes/auth.php
+++ routes/auth.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WorkshopController;
 
 Route::middleware('guest')->group(function () {
     // Route::get('register', [RegisteredUserController::class, 'create'])
@@ -53,4 +54,9 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
+    // Admin middleware group for workshop management
+    Route::middleware(['admin'])->group(function () {
+        Route::resource('workshops', WorkshopController::class);
+    });
 });
