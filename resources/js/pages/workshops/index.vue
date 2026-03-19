7e3947cf532ae1af6c8c854cc6fd5d93f06d0a2f
forklein <giuseppepisani2@libero.it>
2026-03-19 15:14:42 +0100

    Set index component

---
+++ resources/js/pages/workshops/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    interface Workshop {
+        id: number;
+        title: string;
+        description: string;
+        starts_at: string;
+        capacity: number;
+    }
+
+    const props = defineProps<{
+        workshops: Workshop[];
+    }>();
+</script>
+
+<template>
+    <ul>
+        <li v-for="w in workshops" :key="w.id">
+            {{ w.title }}
+        </li>
+    </ul>
+</template>
